d63829d5af7593f2db71fdf8181a41375791181a
Nathan Leibowitz <nathan.leibowitz1@gmail.com>
2019-08-08 15:46:16 -0600

    mobile changes

--- source/components/hero/hero.js
+++ source/components/hero/hero.js
@@ -9,19 +9,26 @@
       $(window).on('scroll', _.throttle(_.bind(this.doParallax, this), updateInterval));
     }
     doParallax(){
-		  var elTop = $(this.element).offset().top;
-		  var scrollTop = $(window).scrollTop();
-		  var winHeight = $(window).height();
-      if((scrollTop + winHeight) > elTop){
-        var plxAmount = (scrollTop + winHeight - elTop) / plxSpeedDiv;
-        var transformString = 'translate3d(0px,'+plxAmount+'px, 0px)';
-        var bg = $(this.element).find('.hero__bg')[0];
-        bg.style.webkitTransform = transformString;
-        bg.style.mozTransform = transformString;
-        bg.style.msTransform = transformString;
-        bg.style.oTransform = transformString;
-        bg.style.transform = transformString;
-      }
+      var windowWidth =$(window).width();
+      console.log(windowWidth + 'width');
+        if (windowWidth > 375){               
+          var elTop = $(this.element).offset().top;
+          var scrollTop = $(window).scrollTop();
+          var winHeight = $(window).height();
+          if((scrollTop + winHeight) > elTop){
+            var plxAmount = (scrollTop + winHeight - elTop) / plxSpeedDiv;
+            var transformString = 'translate3d(0px,'+plxAmount+'px, 0px)';
+            var bg = $(this.element).find('.hero__bg')[0];
+            bg.style.webkitTransform = transformString;
+            bg.style.mozTransform = transformString;
+            bg.style.msTransform = transformString;
+            bg.style.oTransform = transformString;
+            bg.style.transform = transformString;
+          }
+        }
+        else{
+          return
+        }
 		}
   }
   elmtry.bind(Hero, '.hero');
